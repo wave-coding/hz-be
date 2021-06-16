f82f808bcae51d27737f381ea9402f890c417e71
Zoheir Khonyagar <zoheirkhonyagar@gmail.com>
2021-06-16 07:37:15 +0430

    feat: added typescript configs

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,18 @@
 import express, { Request, Response, NextFunction } from "express";
-import todoRoutes from "./routes/todos";
 
-const app = express();
+const port = 3000;
 
-app.use(express.json());
+export const server = async () => {
+  const app = express();
 
-app.use("/todos", todoRoutes);
+  app.use(express.json());
 
-// Error Handling Middleware Function
-app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-  res.status(500).json({ message: err.message });
-});
+  // Error Handling Middleware Function
+  app.use((err: Error, _req: Request, res: Response, _next: NextFunction) => {
+    res.status(500).json({ message: err.message });
+  });
 
-app.listen(3000);
+  app.listen(port, () => {
+    console.log(`App is running on port ${port}`);
+  });
+};
